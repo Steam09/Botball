5a14fa0704f48b88def6dd6e0eb584b9da7e6db1
Steam09 <82530483+Steam09@users.noreply.github.com>
2024-02-27 20:55:12 -0500

    Update legoMain.c

--- legoMain.c
+++ legoMain.c
@@ -11,7 +11,7 @@ int sideServo = 0;
 int rotateServo = 1;
 int clawServo = 2; 
 int lightSensor = 0;
-
+int etSensor = 1;
 
 // Moving Times or Speeds
 int turningTime90 = 4150;
@@ -26,7 +26,7 @@ int closedClaw = 400;
 int innerSide = 200;
 int pushedSide = 1500;
 
-void driveForward(int centimeters);
+void driveForward(float inches, int leftSpeed, int rightSpeed);
 void turnLeft(int degrees);
 void turnRight(int degrees);
 
@@ -43,45 +43,94 @@ int main() {
 
     // Waiting for Start Light
     wait_for_light(lightSensor);
-
-
+    shut_down_in(118);
+    
     // Push 4 intiail left side habitats to Area 1
     enable_servos();
     set_servo_position(sideServo, pushedSide);
     msleep(50);
     set_servo_position(sideServo, innerSide);
 
-    // Go forward to poms and sort
+    // Line up with tape
+    while (analog(0) < 2800) {
+        mav(0,400);
+        mav(1,400);
+        msleep(50);
+    }
+    ao();
+
+    // Sweep top rocks to Area 5 Intersection
 
 
 
-    // Sweep top rocks to Area 5
+
+    driveForward(22, leftVelocity, rightVelocity);
+    while (analog(lightSensor) < 2800) { 
+        driveForward(0.2, 100, 100)
+    }
+
+
+    
+    // Light Switch
+    cmpc(0);
+    set_servo_position(rotateServo, 1400);
+    while (analog(etSensor) < 2800) { 
+        driveForward(0.2, 100, 100)
+    }
+    set_servo_position(rotateServo, 1600);
+    msleep(400);
+    set_servo_position(rotateServo, 1400);
+
+    // Top Cubes
+    driveForward(-4, 500, 500)
+    set_servo_position(rotateServo, verticalArm);
+
     
 
-
 }
 
 
-void driveForward(int time) {
-    mav(1,leftVel);
-    mav(0,rightVel);
-    msleep(time);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void driveForward(float inches, int leftSpeed, int rightSpeed) {
+    mav(1,leftVelocity);
+    mav(0,rightVelocity);
+    msleep(inches);
 }
 
-void turnRight() {
+void turnRight(int degrees) {
     mav(0,-250);
     mav(1,250);
-    msleep(turningTime);
-    mav(0,0);
-    mav(1,0);
+    msleep(degrees * (turningTime90) / 90);
+    ao();
 }
 
-void turnLeft() {
+void turnLeft(int degrees) {
     mav(0,250);
     mav(1,-250);
-    msleep(turningTime);
-    mav(0,0);
-    mav(1,0);
+    msleep(degrees * (turningTime90) / 90);
+    ao();
 }
 
 
