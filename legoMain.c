d71e50680be439051701b8be3a2912c3935a5ab1
Trison Li <82530483+Steam09@users.noreply.github.com>
2024-02-29 12:18:02 -0500

    Update legoMain.c
    testing

--- legoMain.c
+++ legoMain.c
@@ -4,12 +4,12 @@
 clock_t start = clock(), diff;
 diff = clock() - start;
 int msec = diff * 1000 / CLOCKS_PER_SEC;
-printf("Time taken is %d seconds and %d milliseconds", msec/1000, msec%1000); 
+printf("Time taken is %d seconds and %d milliseconds", msec / 1000, msec % 1000);
 
 // Ports
 int sideServo = 0;
 int rotateServo = 1;
-int clawServo = 2; 
+int clawServo = 2;
 int lightSensor = 0;
 int etSensor = 1;
 
@@ -24,13 +24,17 @@ int verticalArm = 800;
 int openClaw = 20;
 int closedClaw = 400;
 int innerSide = 200;
+hey
+
+
 int pushedSide = 1500;
 
 void driveForward(float inches, int leftSpeed, int rightSpeed);
 void turnLeft(int degrees);
 void turnRight(int degrees);
 
-int main() {
+int main()
+{
 
     // Resetting Servos
     enable_servos();
@@ -44,7 +48,7 @@ int main() {
     // Waiting for Start Light
     wait_for_light(lightSensor);
     shut_down_in(118);
-    
+
     // Push 4 intiail left side habitats to Area 1
     enable_servos();
     set_servo_position(sideServo, pushedSide);
@@ -52,9 +56,10 @@ int main() {
     set_servo_position(sideServo, innerSide);
 
     // Line up with tape
-    while (analog(0) < 2800) {
-        mav(0,400);
-        mav(1,400);
+    while (analog(0) < 2800)
+    {
+        mav(0, 400);
+        mav(1, 400);
         msleep(50);
     }
     ao();
@@ -63,17 +68,19 @@ int main() {
 
 
 
+
+
     driveForward(22, leftVelocity, rightVelocity);
-    while (analog(lightSensor) < 2800) { 
+    while (analog(lightSensor) < 2800)
+    {
         driveForward(0.2, 100, 100)
     }
 
-
-    
     // Light Switch
     cmpc(0);
     set_servo_position(rotateServo, 1400);
-    while (analog(etSensor) < 2800) { 
+    while (analog(etSensor) < 2800)
+    {
         driveForward(0.2, 100, 100)
     }
     set_servo_position(rotateServo, 1600);
@@ -82,59 +89,31 @@ int main() {
 
     // Top Cubes
     driveForward(-4, 500, 500)
-    set_servo_position(rotateServo, verticalArm);
-
-    
-
+        set_servo_position(rotateServo, verticalArm);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void driveForward(float inches, int leftSpeed, int rightSpeed) {
-    mav(1,leftVelocity);
-    mav(0,rightVelocity);
+void driveForward(float inches, int leftSpeed, int rightSpeed)
+{
+    mav(1, leftVelocity);
+    mav(0, rightVelocity);
     msleep(inches);
 }
 
-void turnRight(int degrees) {
-    mav(0,-250);
-    mav(1,250);
+void turnRight(int degrees)
+{
+    mav(0, -250);
+    mav(1, 250);
     msleep(degrees * (turningTime90) / 90);
     ao();
 }
 
-void turnLeft(int degrees) {
-    mav(0,250);
-    mav(1,-250);
+void turnLeft(int degrees)
+{
+    mav(0, 250);
+    mav(1, -250);
     msleep(degrees * (turningTime90) / 90);
     ao();
 }
-
-
-
-
 
 int main()
 {
@@ -142,52 +121,51 @@ int main()
     set_servo_position(0, horizontalArm);
     set_servo_position(1, openArm);
     msleep(2000);
-        cmpc(0);
+    cmpc(0);
     cmpc(1);
-    while (analog(0) < 2800) {
-        
-        mav(0,500);
-        mav(1,500);
-        
+    while (analog(0) < 2800)
+    {
+
+        mav(0, 500);
+        mav(1, 500);
+
         printf("sensor reads %d\n", analog(0));
-        
+
         msleep(100);
     }
-    
-    
-    mav(0,500);
-    mav(1,500);
+
+    mav(0, 500);
+    mav(1, 500);
     msleep(800);
     int driveTime = gmpc(1);
     printf("%d\n", driveTime);
     ao();
-    
+
     msleep(2000);
-    
-    
+
     set_servo_position(1, closedArm);
     msleep(2000);
     set_servo_position(0, maxHeightArm);
     msleep(2000);
     turnLeft();
-        printf("%d\n",driveTime);
+    printf("%d\n", driveTime);
     turnLeft();
-    
+
     cmpc(0);
     cmpc(1);
-    while (gmpc(0) < driveTime) {   //2800 ticks = 333 mm  
-        mav(0,500);
-        mav(1,500);
+    while (gmpc(0) < driveTime)
+    { // 2800 ticks = 333 mm
+        mav(0, 500);
+        mav(1, 500);
     }
-        printf("STOP");
-    mav(0,0);
-    mav(1,0);
+    printf("STOP");
+    mav(0, 0);
+    mav(1, 0);
     msleep(2000);
-    
+
     set_servo_position(1, openArm);
     msleep(2000);
     disable_servos();
-        printf("%d\n",driveTime);
+    printf("%d\n", driveTime);
     ao();
 }
-
