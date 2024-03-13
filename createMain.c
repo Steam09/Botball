294533e3f270097d39046c5ffa5252e6a014de74
GregoryEscobar <161763701+GregoryEscobar@users.noreply.github.com>
2024-03-13 08:22:49 -0400

    Update createMain.c

--- createMain.c
+++ createMain.c
@@ -10,15 +10,46 @@ printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
 
 create3_pose_get();
 
+// March 11, 2024 CREATE
+int main()
+{
+// functions 
+    void drive_forward(int distance) {}
+    void turn_left(void) {}
+    void turn_right(void) {}
+    void drive_backward(int distance) {}
+    void raise_claw(int angle) {}
+    void lower_claw(int angle) {}
+    void raise_plow(int angle) {}
+    void lower_plot(int angle) {}    
 
-int main() {
-    create_connect();
-    enable_servos();
-    cmpc(0);
-    cmpc(1);
-    set_servo_position();
-    set_servo_position();
+// variables
+    int speed = __; 
 
 
+// 0-15 seconds 
+// drive out of starting box and sort poms CREATE
+// turn in starting box and push habitats into area 1 LEGO
+    
+// 15-30 seconds
+// push rocks into area 6 CREATE
+// push rocks into area 6 LEGO
+
+// 30-45 seconds
+// push fuel into area 4 CREATE
+// 
+    
+// 45-60 seconds
+// push rocks into Rock Heap
+
+//60-90 seconds
+// flip solar panel
+
+// 90-105 seconds
+// Margin of error
+   
+// 105-120 seconds
+// return to starting box
 
 }
+
