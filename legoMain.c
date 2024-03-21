611e52e01def47df0a93b5ad4c3817c9a5f13cd5
Trison Li <82530483+Steam09@users.noreply.github.com>
2024-03-21 07:56:21 -0400

    legoBot_change_orientation (#13)
    * legoBot change orientation
    
    Making it do the multiplier THEN poms to give the roomba enough time to move out the way. This is due to the fact that the current code has the legoBot at the poms in < 12 seconds, which I doubt we can get the roomba to get the sort in that time sooo... :)
    
    * Update legoMain.c
    
    New code within the functions so it works in C, testing movement and angles (this is so scuffed).
    
    * Update legoMain.c

--- legoMain.c
+++ legoMain.c
@@ -6,6 +6,8 @@ const int rotateServo = 0;
 const int clawServo = 1; 
 const int lightSensor = 0;
 const int etSensor = 1;
+const int leftButton = 0;
+const int rightButton = 1;
 
 // Moving Times or Speeds
 int turningTime90 = 1250;
@@ -15,7 +17,7 @@ const int tapeIndicator = 3850;
 // 26.5 cm per 1000ms
 
 // Servo Positions
-int horizontalArm = 1473;
+int horizontalArm = 500;
 int verticalArm = 1321;
 int openClaw = 1731;
 int closedClaw = 1026;
@@ -23,22 +25,25 @@ int innerSide = 200;
 int pushedSide = 1500;
 
 void driveForward(float cm, int leftSpeed, int rightSpeed);
+void driveBackward(float cm, int leftSpeed, int rightSpeed);
 void turnLeft(int degrees);
 void turnRight(int degrees);
 void alignTape(int speed);
-void servoSpeed(int speed, int endPosition)
+void servoSpeed(int port, int speed, int initialPosition, int endPosition);
+void alignWall(void);
 
 int main() {
 
     // Resetting Servos
-    	enable_servos();
-    	cmpc(0);
-    	cmpc(1);
-    	set_servo_position(sideServo, innerSide);
-    	set_servo_position(rotateServo, verticalArm);
-    	set_servo_position(clawServo, closedClaw);
-    	disable_servos();
+    enable_servos();
+    cmpc(0);
+    cmpc(1);
+    set_servo_position(sideServo, innerSide);
+    set_servo_position(0, 1450);
+    set_servo_position(1, 200);
+    msleep(1000);
 
+    /*
     // Waiting for Start Light
     	wait_for_light(lightSensor);
     	shut_down_in(118);
@@ -48,63 +53,71 @@ int main() {
     	set_servo_position(sideServo, pushedSide);
     	msleep(50);
     	set_servo_position(sideServo, innerSide);
-
-    // Line up with tape
-    	alignTape(250);
-
-    // Sweep top rocks to Area 5 Intersection
-    	driveForward(75, leftVelocity-100, rightVelocity);
-    	turnRight(80);
-    	driveForward(18, leftVelocity, rightVelocity);
-    	turnRight(30);
-    	driveForward(10, leftVelocity, rightVelocity);
-    	driveForward(20, -1 * leftVelocity, -1 * rightVelocity);
-
-    // Red Poms
-    	turnRight(60);
-    	driveForward(20, leftVelocity, rightVelocity);
-    	turnLeft(40);
-	driveForward(27, leftVelocity, rightVelocity);
-    	driveForward(20, -1 * leftVelocity, -1 * rightVelocity);
-    	alignTape(-250);
-    
-    // Light Switch
-    /*
-    	turnLeft(90);
-	alignTape(500);
-	driveForward(10, leftVelocity, rightVelocity);
-    	turnLeft(90);
     */
 
-    // Top Cubes
-    	turnLeft(90);
-    	alignTape(500);
-	driveForward(12, leftVelocity, rightVelocity);
-    	turnLeft(95);
-    	msleep(500);
-    	set_servo_position(0, 1250);
-    	msleep(500);
-    	set_servo_position(0, 1210);
-    	msleep(500);
-    	set_servo_position(1, 1603);
-    	msleep(1000);
-    	driveForward(25, leftVelocity, rightVelocity);
-    	msleep(1000);
-    	set_servo_position(1, 1024);
-    	msleep(500);
-    	driveForward(10, -1 * leftVelocity, -1 * rightVelocity); // has grabb yey
-    
+    // Line up with tape
+    alignTape(750);
+
+    // Sweep top rocks to Area 5 Intersection
+    driveForward(75, leftVelocity-100, rightVelocity);  // Arc to hit the top rock
+    turnRight(80);
+    driveForward(22, leftVelocity, rightVelocity);
+    turnRight(30);
+    driveForward(8, leftVelocity, rightVelocity);
+    driveBackward(20, leftVelocity, rightVelocity);
+    msleep(500);
+    turnRight(100); // CCURENT
+    driveForward(10, leftVelocity, rightVelocity);
+    turnRight(180);
+    driveForward(10, leftVelocity, rightVelocity);
+    turnLeft(75);
+    msleep(500); 
+
+    // multis
+
+    servoSpeed(0, 5, verticalArm, 1240);
+    set_servo_position(1, 1603);
+    msleep(200);
+    driveForward(15, leftVelocity, rightVelocity);
+    msleep(1000);
+    set_servo_position(1, 300);
+    msleep(500);
+    driveBackward(20, leftVelocity, rightVelocity); 
+    turnLeft(130);
+    msleep(500);
+
+    // Outer Poms
+
+    driveForward(10, leftVelocity - 200, rightVelocity); 
+    msleep(500);
+    driveBackward(20, leftVelocity, rightVelocity); 
+    msleep(500);
+    servoSpeed(0, 4, 1240, 300);
+    msleep(1000);
+    set_servo_position(clawServo, openClaw);
+    msleep(500);
+    servoSpeed(0, 1, horizontalArm, verticalArm);
+
+    // Inner Poms
+
+    turnRight(40);
+    driveForward(30, leftVelocity, rightVelocity - 900); 
+    driveBackward(12, leftVelocity, rightVelocity - 900); 
+    turnRight(130);
+    driveForward(50, leftVelocity, rightVelocity); 
+    alignTape(1000);
+
 
     
 
 }
 
 // Changes how fast the servo goes, otherwise we're gonna make the robot do a kickflip. 
-void servoSpeed(int port, int speed, int intialPosition, int endPosition) {
-	increment = abs((endPosition - intialPosition) / speed);
-	if (endPosition > intialPosition) {
-		for( i = 0; i <= speed; i += 1 ){
-			set_servo_position(port, intialPosition + increment * i);
+void servoSpeed(int port, int speed, int initialPosition, int endPosition) {
+	int increment = abs((endPosition - initialPosition) / speed);
+	if (endPosition > initialPosition) {
+		for( int i = 0; i <= speed; i += 1 ){
+			set_servo_position(port, initialPosition + increment * i);
 			msleep(50);
 			if (initialPosition + increment * i > endPosition) {
 				set_servo_position(port, endPosition);
@@ -115,7 +128,7 @@ void servoSpeed(int port, int speed, int intialPosition, int endPosition) {
 		}
 	}
 	else {
-		for( i = 0; i < speed; i += 1 ){
+		for( int i = 0; i < speed; i += 1 ){
 			set_servo_position(port, initialPosition - increment * i);
 			msleep(50);
 			if (initialPosition - increment * i < endPosition) {
@@ -127,7 +140,6 @@ void servoSpeed(int port, int speed, int intialPosition, int endPosition) {
 		}
 	}
 	enable_servos();
-	return;
 }
 
 // Goes up to the nearest tape in front of it at speed "int speed". 
@@ -140,12 +152,18 @@ void alignTape(int speed) {
     freeze(0);
     freeze(1);
     msleep(100);
-	return();
 }
 
 void driveForward(float cm, int leftSpeed, int rightSpeed) {
     mav(1,leftSpeed);
     mav(0,rightSpeed);
+    msleep(1000/15 * cm);
+    ao();
+}
+
+void driveBackward(float cm, int leftSpeed, int rightSpeed) {
+    mav(1,(-1) * leftSpeed);
+    mav(0,(-1) * rightSpeed);
     msleep(1000/15 * cm);
     ao();
 }
@@ -164,3 +182,19 @@ void turnLeft(int degrees) {
     ao();
 }
 
+void alignWall(void) {
+    while ((leftButton != 1) && (rightButton != 1)) {
+		if (leftButton == 1) {
+			driveBackward(1, leftVelocity - 800, rightVelocity);
+			msleep(10);
+		}
+		else if (rightButton == 1) {
+			driveBackward(1, leftVelocity, rightVelocity - 800);
+			msleep(10);
+		}
+		else {
+			driveBackward(1, leftVelocity, rightVelocity);
+			msleep(10);
+		}
+	}
+}
