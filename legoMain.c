ec124937974baf9703575c2c2317717919bb7d1c
Trison Li <82530483+Steam09@users.noreply.github.com>
2024-04-15 15:46:40 -0400

    Update legoMain.c
    New Order of events (again...), trying to create a grabbing habitat function

--- legoMain.c
+++ legoMain.c
@@ -3,11 +3,11 @@
 // Servos
 const int rotateServo = 0;
 const int clawServo = 1;
-// Analog
+// Analog Ports
 const int backLightSensor = 0;
 const int leftLightSensor = 4;
 const int rightLightSensor = 5;
-// Digital
+// Digital Ports
 const int backLeftButton = 0;
 const int backRightButton = 1;
 
@@ -20,21 +20,22 @@ const int tapeIndicator = 3950;
 
 // Servo Positions
 int horizontalArm = 100;
-int verticalArm = 1321;
-int openClaw = 1731;
-int closedClaw = 1026;
+int verticalArm = 1500;
+int openClaw = 1300;
+int closedClaw = 200;
 
-int positions = [1000,1000,1000,1000,1000,1000];
+int positions[] = {521,693,809,1020,1207};
+const int armLength = 1000;
 
 void driveForward(float cm, int leftSpeed, int rightSpeed);
 void driveBackward(float cm, int leftSpeed, int rightSpeed);
 void turnLeft(int degrees);
 void turnRight(int degrees);
 void alignTape(int speed);
-void servoSpeed(int port, int speed, int initialPosition, int endPosition);
+void servoSpeed(int port, int speed, int endPosition);
 void alignWall(void);
-void habitats(int poleNum, int num);
-
+void habitats(int poleNum, int num, int side);
+void grabHabitat(int newNum);
 
 int main() {
 
@@ -42,125 +43,98 @@ int main() {
     enable_servos();
     cmpc(0);
     cmpc(1);
-    servoSpeed(rotateServo, 40, get_servo_position(rotateServo), 1400);
-    servoSpeed(clawServo, 20, get_servo_position(clawServo), 1600);
-    servoSpeed(clawServo, 20, 1600, 200);
+    servoSpeed(rotateServo, 40, 1400);
+    servoSpeed(clawServo, 20, 1600);
+    servoSpeed(clawServo, 20, 200);
     printf("calibrating\n");
     msleep(100);
-
-
-    /*
-    wait_for_light(lightSensor);
+	/*
+    wait_for_light(backLightSensor);
     shut_down_in(119);
-    */
-
-  
+  	*/
     // Line up with tape
-    alignTape(400);
+    alignTape(600);
 
     // Sweep top rocks to Area 5 Intersection
     printf("Rocks\n");
     driveForward(80, leftVelocity-100, rightVelocity);  // Arc to hit the top rock
     turnRight(80);
-    driveForward(25, leftVelocity, rightVelocity); //originally 22
+    driveForward(25, leftVelocity, rightVelocity); // Rocks to intersection
 
     driveBackward(10, leftVelocity, rightVelocity);
-    turnRight(60);
+    turnRight(60); 
     driveBackward(8, leftVelocity, rightVelocity);
-    msleep(300);
     
     turnRight(43);
     printf("Align Tape\n");
     while (analog(rightLightSensor) < tapeIndicator) {
         printf("%d \n", analog(5));
-        driveForward(5, 300, -300);
+        driveForward(5, 400, -400);
         msleep(5);
     }
     ao();
-    msleep(300);
+    msleep(50);
 
-    turnRight(40);
-  
+    turnRight(40); // Align to middle multis
+    servoSpeed(clawServo, 5, 1500);
+    servoSpeed(rotateServo, 30, 1060);
     msleep(200);
-    servoSpeed(clawServo, 20, 200, 1500);
-    servoSpeed(rotateServo, 30, 1450, 1060);
-    msleep(200);
-    driveForward(8, leftVelocity, rightVelocity);
-    msleep(100);
-    servoSpeed(clawServo, 20, 1500, 200);
+    driveForward(9, leftVelocity, rightVelocity);
+    servoSpeed(clawServo, 20, 200);
     printf("Grabbed Center\n");
-    msleep(100);
-    driveBackward(13, leftVelocity, rightVelocity);
+    msleep(50);
+    driveBackward(5, leftVelocity, rightVelocity);
 
-    turnLeft(60);
-    driveBackward(15, leftVelocity, rightVelocity);
-    servoSpeed(rotateServo, 40, get_servo_position(rotateServo), 300);
-    msleep(500);
-    servoSpeed(clawServo, 6, 200, 1500);
-    msleep(200);
-    servoSpeed(rotateServo, 40, 300, 1400);
-    
+    turnLeft(90);
+    driveForward(15, leftVelocity, rightVelocity);
+    turnLeft(90);
+    servoSpeed(rotateServo, 40, 300);
     msleep(300);
-    turnLeft(12);
+    servoSpeed(clawServo, 6 1500); // Place multipliers on the ground
+    msleep(50);
+    servoSpeed(rotateServo, 40, 1400);
+    msleep(300);
+    turnLeft(8);
+
     printf("splitting multis\n");
-    driveForward(38, 1400, 1500);
-    driveForward(24, 0, 1500);
     driveForward(20, leftVelocity, rightVelocity);
-    
-    driveBackward(15, leftVelocity, rightVelocity);
-    turnRight(90);
+    turnRight(8);
+    driveForward(12, leftVelocity, rightVelocity);
+    driveBackward(10, leftVelocity, rightVelocity);
+    turnRight(120);
+
     printf("left poms\n");
-    driveForward(5, leftVelocity, rightVelocity);
-    driveForward(40, leftVelocity, 200);
     driveForward(10, leftVelocity, rightVelocity);
-    driveForward(22, 600, rightVelocity);
-    driveForward(20, leftVelocity, rightVelocity);
+    driveForward(15, leftVelocity, 200);
+    driveForward(10, 200, rightVelocity)
+    driveForward(30, leftVelocity, rightVelocity);
 
     driveBackward(20, 1500, 1500);
-    turnRight(46);
-    printf("to diagonal\n");
-    // go up to fuel area
-    servoSpeed(rotateServo, 30, 1400, 1300);
-    msleep(200);
-    driveForward(80, leftVelocity, rightVelocity);
-    servoSpeed(clawServo, 6, get_servo_position(clawServo), 1243);
-    turnLeft(115);
+    turnLeft(90);
+    driveForward(10, 1500, 1500);
     
-    servoSpeed(rotateServo, 20, get_servo_position(rotateServo), 1190);
-    driveForward(39, leftVelocity, rightVelocity); 
-    servoSpeed(clawServo, 6, get_servo_position(clawServo), 100);
-    driveBackward(15, leftVelocity, rightVelocity);
-    turnRight(155);
-    driveForward(20, 1500, 1500);
-    turnLeft(25);
-
-    // Drop multiplier rocks
-    servoSpeed(rotateServo, 40, 1190, 300);
-    msleep(400);
-    servoSpeed(clawServo, 20, get_servo_position(clawServo), 1243);
-    msleep(100);
-    servoSpeed(rotateServo, 40, 300, 1300);
-    msleep(400);
-    driveBackward(10, leftVelocity, rightVelocity);
-
+    /*
     turnRight(150);
     driveForward(50, leftVelocity, rightVelocity);
     turnRight(30);
     driveForward(50, leftVelocity, rightVelocity);
-
-    /*
-    servoSpeed(clawServo, 10, get_servo_position(clawServo), 1200);
+	doubleTape();
+    servoSpeed(clawServo, 10, 1200);
     for (int i = 0; i < 6; i += 1) {
         habitats(i, 6 - i);
     }
     */
+   
+    disable_servos();
+    ao();
     printf("I snooze\n");
 
 }
 
 
-// Changes how fast the servo goes, otherwise we're gonna make the robot do a kickflip.
-void servoSpeed(int port, int speed, int initialPosition, int endPosition) {
+// Custom Function for a Controlled Servo
+void servoSpeed(int port, int speed, int endPosition) {
+   int initialPosition = get_servo_position(port);
    int increment = abs((endPosition - initialPosition) / speed);
    if (endPosition > initialPosition) {
        for( int i = 0; i <= speed; i += 1 ){
@@ -189,8 +163,7 @@ void servoSpeed(int port, int speed, int initialPosition, int endPosition) {
    enable_servos();
 }
 
-
-// Goes up to the nearest tape in front of it at speed "int speed".
+// Drives forward uptil tape, then stops; Must trigger both sensors
 void alignTape(int speed) {
    while (analog(5) < tapeIndicator) {
        printf("%d \n", analog(5));
@@ -202,7 +175,7 @@ void alignTape(int speed) {
    msleep(100);
 }
 
-
+// Drives forward a set distance cm with custom wheel speeds
 void driveForward(float cm, int leftSpeed, int rightSpeed) {
    mav(1,leftSpeed);
    mav(0,rightSpeed);
@@ -210,7 +183,7 @@ void driveForward(float cm, int leftSpeed, int rightSpeed) {
    ao();
 }
 
-
+// See driveForward, easier for debugging and testing purposes
 void driveBackward(float cm, int leftSpeed, int rightSpeed) {
    mav(1,(-1) * leftSpeed);
    mav(0,(-1) * rightSpeed);
@@ -218,7 +191,7 @@ void driveBackward(float cm, int leftSpeed, int rightSpeed) {
    ao();
 }
 
-
+// Self-Explanatory
 void turnRight(int degrees) {
    mav(0,-1000);
    mav(1,1000);
@@ -226,7 +199,7 @@ void turnRight(int degrees) {
    ao();
 }
 
-
+// Self-Explanatory
 void turnLeft(int degrees) {
    mav(0,1000);
    mav(1,-1000);
@@ -234,7 +207,7 @@ void turnLeft(int degrees) {
    ao();
 }
 
-
+// Drives backwards into a wall until normal
 void alignWall(void) {
    while ((digital(backLeftButton) != 1) || (digital(backRightButton) != 1)) {
        if (digital(backRightButton) == 1) {
@@ -255,32 +228,51 @@ void alignWall(void) {
    }
 }
 
-void habitats(int poleNum, int num) {
+// Drives up to and places habitats on designated poles
+void habitats(int poleNum, int num, int side) { 
 
-    int heights[] = {400, 700, 1000, 700, 400};
-
-    servoSpeed(rotateServo, 40, get_servo_position(rotateServo), positions[num]);
+    servoSpeed(rotateServo, 40, positions[num]);
     driveForward(5,500,500);
-    servoSpeed(clawServo, 10, get_servo_position(clawServo), 100);
+    servoSpeed(clawServo, 10, 100);
 
-    for (int i = 0; i < num * 4; i += 1) {
-        servoSpeed(rotateServo, 25, get_servo_position(rotateServo), position[num] + 50);
-        driveForward(2, leftVelocity, rightVelocity);
-    }
-    servoSpeed(rotateServo, 5, get_servo_position, verticalArm);
+    grabHabitat(0);
+    servoSpeed(rotateServo, 5, verticalArm);
 
     turnRight(180);
     alignWall();
 
+    if (side == 0) {
+        driveForward(poleNum * 5 + 2 , leftVelocity, rightVelocity);
+    }
+    else {
+        driveForward(poleNum * 5 + 2 , leftVelocity, rightVelocity);
+    }
+
+    turnLeft(90);
+
+    servoSpeed(rotateServo, poleNum * 10, heights[poleNum]);
+    servoSpeed(clawServo, 10 1200);
+    servoSpeed(rotateServo, poleNum * 10, verticalArm);
+
+    turnLeft(90);
     driveForward(poleNum * 5 + 2, leftVelocity, rightVelocity);
 
-    turnLeft(90);
+}
 
-    servoSpeed(rotateServo, poleNum * 10, verticalArm, heights[poleNum]);
-    servoSpeed(clawServo, 10, 100, 1200);
-    servoSpeed(rotateServo, poleNum * 10, heights[poleNum], verticalArm);
+// Grabbing the habitats off their poles / grpund
+void grabHabitat(int newNum) {
 
-    turnLeft(90);
-    driveForward(polenum * 5 + 2, leftVelocity, rightVelocity);
+    if (newNum < 4) {
+        servoSpeed(rotateServo, 20, horizontalArm);
+        driveForward(4 * newNum + 5, leftVelocity, rightVelocity);
+        servoSpeed(clawServo, 10, closedClaw);
+        msleep(50);
+        servoSpeed(rotateServo, 30, verticalArm);
+        printf("Grabbed ground multiplier\n")
+    }
 
+    else {
+
+    }
+    
 }
