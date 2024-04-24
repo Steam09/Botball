d77a4bf4ec8a1fdea63b0abe10f8d14086b0e176
angelah2026 <161751612+angelah2026@users.noreply.github.com>
2024-04-24 17:01:43 -0400

    Update createMain.c

--- createMain.c
+++ createMain.c
@@ -1,8 +1,8 @@
 #include <kipr/wombat.h>
 // March 11, 2024 CREATE
 // variables
-int lm_speed = 192; //changed from 192
-int rm_speed = 195; //changed from 195
+int lm_speed = 195; //changed from 192
+int rm_speed = 194; //changed from 195
 
 //declare functions
 void drive_forward(int time);
@@ -18,33 +18,34 @@ int main()
 {
 	create_connect();
     enable_servos();
-    
+    /*
         wait_for_light(0);
-        msleep(9000);
+    */
     
-   
+    //PAUSE TO GIVE LEGOBOT TIME
+    //msleep(15000);
  
     raise_claw_arm(931); // port 0 - 931
     raise_claw(1452); // port 2 - formerly 1831
     open_claw(1800); 
     
     //rocks (TESTED)
-    drive_forward(6800); //drive toward 3 rocks
+    drive_forward(6600); //drive toward 3 rocks
     turn_left(400); // turn to catch 4th rock
     drive_forward(3000); //drive into rock heap and catch 5th rock 
     turn_left(190); // turn toward rock heap
-    drive_forward(1000); // push //CHANGED from 1200
+    drive_forward(700); // push //CHANGED from 1200
     turn_right(120); // turn right to unlatch from rock
-    drive_backward(500); // back up to prepare for poms //CHANGED was 500
-    turn_left(300);//CHANGED was 300
+    drive_backward(700); // back up to prepare for poms
+    turn_left(310);
     drive_backward(1600);
     
     // 2 orange poms (TESTED)
-    turn_left(420); //CHANGED from 420
+    turn_left(415); //CHANGED from 420
     
     // taller green multipliers
     msleep(500);
-    drive_forward(2525); //was 2585 
+    drive_forward(2550); //was 2585 
     msleep(1000);
     raise_claw(1793); //was 1770 CHANGED port 2
     msleep(1000);
@@ -61,7 +62,7 @@ int main()
     
     //split multipliers & move one to fuel area
     turn_right(130);
-    drive_forward(1200);
+    drive_forward(1100);
     
  /*   raise_claw_arm(1000);
     msleep(1000);
@@ -70,53 +71,23 @@ int main()
     drive_forward(1200);
     msleep(900);
 */
-    //pushing third fuel 
-   
-   turn_right(2500); //decrease this turn was 2500 changed
+    //pushing third fuel
+    turn_right(2500); //decrease this turn was 2500 changed
     msleep(1000);
     drive_forward(710); //was 1200 changed
     msleep(500);
     turn_right(300);
-    drive_forward(370);
-    drive_backward(800);
+    //drive_forward(370);
+    drive_backward(600);
     turn_left(800);
     drive_forward(1300);
     turn_right(800);
     msleep(500);
     drive_forward(350);
     drive_backward(500);
-    turn_left(800); //800 to go straight
+    turn_left(1000); //800 to go straight
     msleep(500);
-        
-    //Sorting Poms and aligning for Habitats
-    drive_forward(800);
-    msleep(500);
-    turn_left(800);//left turn towards equitment pole
-    msleep(500);
-    drive_forward(500);//push poms
-    msleep(500);
-    drive_backward(400);//release poms
-    msleep(500);
-    turn_right(800);//turn towards next poms
-    msleep(500);
-    drive_forward(450);//turn below the next poms 
-    msleep(500);
-    turn_right(800);//turn 90 to scoop poms
-    msleep(500);
-    drive_forward(450);//push poms into next area
-    msleep(500);
-    turn_right(400);//turn 45 to align to habitats
-    msleep(500);
-    drive_forward(300);//move to center for habitats 
-    msleep(500);
-    // Grab and latch the habitats
-    //Backup and align to short pole
-    //put habitat on short pole
-    //
     
-        
-    
-    /*
     //pushing green poms into Area 2
     drive_forward(2200); //long distance across board going past moonbase
     msleep(500);
@@ -153,7 +124,7 @@ int main()
     msleep(500); 
     
     
-    */
+    
     //positions claw to pick up habitats
     raise_claw_arm(1702);
     raise_claw(1300); //CHANGEDDDDDDDDDDDDDDDDDDDDDDDD
